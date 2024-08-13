650678a23a4de6faa4de8b7f1d8fd6b9f02f66d7
JafethPonce <JafethPonce06@hotmail.com>
2024-08-12 22:27:16 -0600

    Base de datos actualizada active_users

--- blog.sql
+++ blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-05-2024 a las 01:35:29
+-- Tiempo de generación: 13-08-2024 a las 06:25:53
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 8.2.0
 
@@ -24,6 +24,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `active_sessions`
+--
+
+CREATE TABLE `active_sessions` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `session_id` varchar(255) NOT NULL,
+  `start_time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `active_sessions`
+--
+
+INSERT INTO `active_sessions` (`id`, `user_id`, `session_id`, `start_time`) VALUES
+(1, 5, 't2vakjq0tbvbjho9ehgjte0lhc', '2024-07-18 01:54:25'),
+(2, 5, 't2vakjq0tbvbjho9ehgjte0lhc', '2024-07-18 02:01:58'),
+(3, 6, 't2vakjq0tbvbjho9ehgjte0lhc', '2024-07-18 02:08:37'),
+(4, 2, 't2vakjq0tbvbjho9ehgjte0lhc', '2024-07-20 04:07:30'),
+(5, 2, 't2vakjq0tbvbjho9ehgjte0lhc', '2024-07-20 04:34:28');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `info_user`
 --
 
@@ -35,17 +59,21 @@ CREATE TABLE `info_user` (
   `psw` varchar(255) NOT NULL,
   `id_infouser` int(11) NOT NULL,
   `id_rol` int(11) NOT NULL,
-  `id_nota` int(11) NOT NULL
+  `id_nota` int(11) NOT NULL,
+  `pregunta_secreta` varchar(255) NOT NULL,
+  `respuesta_secreta` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `info_user`
 --
 
-INSERT INTO `info_user` (`nombre`, `apellidos`, `celular`, `email`, `psw`, `id_infouser`, `id_rol`, `id_nota`) VALUES
-('Jafet', 'Ponce', '8445434438', 'jafethponce06@hotmail.com', 'Jafet123456', 1, 2, 0),
-('Ivan', 'Prueba', '8445986953', 'Ivan@prueba.com', '123456789', 2, 1, 0),
-('prueba', 'prueba', '1234567895', 'ajajaj@gmail.com', '11111111', 3, 2, 0);
+INSERT INTO `info_user` (`nombre`, `apellidos`, `celular`, `email`, `psw`, `id_infouser`, `id_rol`, `id_nota`, `pregunta_secreta`, `respuesta_secreta`) VALUES
+('Genaro', 'Osoria', '8441236589', 'OsoriaGenaro@gmail.com', '$2y$10$lEb6uRdmY.F6DQfi2VAni.GFD.UZELUgosZSLN3Hogy4d52mnLype', 1, 2, 0, 'ciudad', 'Saltillo'),
+('Jafet', 'Ponce', '8444845578', 'jafethponce06@hotmail.com', '$2y$10$vF/JIBYtRIwwL23IR3ki6ewFX6oJnlrnK9yb3TXsyN/QB0eMJwKZ.', 2, 2, 0, '¿Cuál es el nombre de tu primera mascota?', 'Yaki'),
+('Administrador', 'Admin', '844545565', 'Admin@blog.com', 'AdminBlog', 3, 1, 0, 'No hay', 'No hay'),
+('David', 'Amador', '5654334587', 'DavidAmador@amador.com', '$2y$10$W4y5p1N0QJcif7uA7vTBfuv47SNprqj0QV0kfMarbJT3.CcSbUJNm', 5, 2, 0, '¿Cuál es el nombre de tu escuela primaria?', 'Margarita Maza de Juarez'),
+('Ivan', 'Perez', '1234567896', 'IvanPerez@gmail.com', '$2y$10$5KxBRrE/HxwPnZPsOat2iOB.Jm.L.TtStlqzZ.i7DKEvg6hjLy8/6', 6, 2, 0, '¿En qué ciudad naciste?', 'Saltillo');
 
 -- --------------------------------------------------------
 
@@ -70,7 +98,9 @@ INSERT INTO `notas` (`Nombre`, `Nota`, `Fecha`, `id_nota`) VALUES
 ('Jafet', 'nosexd', '0000-00-00', 3),
 ('Jafet', 'prueba2', '2024-05-28', 4),
 ('Jafet', 'bjguygyuguygygygy', '2024-05-28', 5),
-('prueba', 'holi', '2024-05-28', 6);
+('prueba', 'holi', '2024-05-28', 6),
+('Jafet', 'Hola', '2024-06-27', 7),
+('Genaro', 'Hola este es un comentario', '2024-06-27', 8);
 
 -- --------------------------------------------------------
 
@@ -96,6 +126,13 @@ INSERT INTO `rol` (`id_rol`, `rol`) VALUES
 --
 
 --
+-- Indices de la tabla `active_sessions`
+--
+ALTER TABLE `active_sessions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- Indices de la tabla `info_user`
 --
 ALTER TABLE `info_user`
@@ -119,16 +156,22 @@ ALTER TABLE `rol`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `active_sessions`
+--
+ALTER TABLE `active_sessions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT de la tabla `info_user`
 --
 ALTER TABLE `info_user`
-  MODIFY `id_infouser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_infouser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `notas`
 --
 ALTER TABLE `notas`
-  MODIFY `id_nota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_nota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `rol`
@@ -139,6 +182,12 @@ ALTER TABLE `rol`
 --
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `active_sessions`
+--
+ALTER TABLE `active_sessions`
+  ADD CONSTRAINT `active_sessions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `info_user` (`id_infouser`);
 
 --
 -- Filtros para la tabla `info_user`
